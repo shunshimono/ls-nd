e75fe6997a94b998f5317aee1632640410825eae
shun shimono <shunshun5589@gmail.com>
2020-12-26 17:30:02 +0900

    Cookie

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
-const qs = require("querystring"); //★追加
+const qs = require("querystring");
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
 const other_page = fs.readFileSync("./other.ejs", "utf8");
@@ -24,15 +24,15 @@ console.log("Server start!");
  * @param {Object} response http.ServerResponse
  */
 function getFromClient(request, response) {
-  var url_parts = url.parse(request.url, true); //★trueに
+  var url_parts = url.parse(request.url, true);
 
   switch (url_parts.pathname) {
     case "/":
-      response_index(request, response); //★修正
+      response_index(request, response);
       break;
 
     case "/other":
-      response_other(request, response); //★修正
+      response_other(request, response);
       break;
 
     case "/style.css":
@@ -48,16 +48,63 @@ function getFromClient(request, response) {
   }
 }
 
-// ★indexのアクセス処理
+var data = { msg: "no message..." };
+
+// indexのアクセス処理
 function response_index(request, response) {
-  var msg = "これはIndexページです。";
+  // POSTアクセス時の処理
+  if (request.method == "POST") {
+    var body = "";
+
+    // データ受信のイベント処理
+    request.on("data", (data) => {
+      body += data;
+    });
+
+    // データ受信終了のイベント処理
+    request.on("end", () => {
+      data = qs.parse(body);
+      // クッキーの保存
+      setCookie("msg", data.msg, response);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+// indexのページ作成
+function write_index(request, response) {
+  var msg = "※伝言を表示します。";
+  var cookie_data = getCookie("msg", request);
   var content = ejs.render(index_page, {
     title: "Index",
     content: msg,
+    data: data,
+    cookie_data: cookie_data,
   });
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write(content);
   response.end();
+}
+
+// クッキーの値を設定
+function setCookie(key, value, response) {
+  var cookie = escape(value);
+  response.setHeader("Set-Cookie", [key + "=" + cookie]);
+}
+// クッキーの値を取得
+function getCookie(key, request) {
+  var cookie_data =
+    request.headers.cookie != undefined ? request.headers.cookie : "";
+  var data = cookie_data.split(";");
+  for (var i in data) {
+    if (data[i].trim().startsWith(key + "=")) {
+      var result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return "";
 }
 
 // ★otherのアクセス処理
