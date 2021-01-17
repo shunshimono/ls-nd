a7ee8d1ffee950ef5896926d7cf588200eddfc81
shun shimono <shunshun5589@gmail.com>
2021-01-17 22:32:54 +0900

    ts導入

--- App.js
+++ App.js
@@ -1,120 +1,99 @@
-const http = require("http");
-const fs = require("fs");
-const ejs = require("ejs");
-const url = require("url");
-const qs = require("querystring");
-
-// サーバー側でファイルを読み込みをさせる
-const index_page = fs.readFileSync("./index.ejs", "utf8");
-const other_page = fs.readFileSync("./other.ejs", "utf8");
-const style_css = fs.readFileSync("./style.css", "utf8");
-
-const server = http.createServer(accessFromClient);
-
-server.listen(4000);
-
-console.log("Start Server!!");
-
-function accessFromClient(request, response) {
-  // trueにすることでクエリーパラメータを受け取ることが可能となる
-  const url_parts = url.parse(request.url, true);
-  switch (url_parts.pathname) {
-    case "/":
-      responseIndex(request, response);
-      break;
-    case "/other":
-      responseOther(request, response);
-      break;
-    case "/style.css":
-      response.writeHead(200, { "Content-Type": "text/css" });
-      response.write(style_css);
-      response.end();
-      break;
-    default:
-      response.writeHead(200, { "Content-Type": "text/plain" });
-      response.end("no page....");
-      break;
-  }
-}
-
-var data = {
-  msg: "no message...",
-};
-
-function responseIndex(request, response) {
-  if (request.method == "POST") {
-    var body = "";
-    request.on("data", (data) => {
-      body += data;
-    });
-    request.on("end", () => {
-      data = qs.parse(body);
-      setCookie("msg", data.msg, response);
-      white_index(request, response);
-    });
-  } else {
-    white_index(request, response);
-  }
-}
-
-function white_index(request, response) {
-  var msg = "※伝言を表示します。";
-  var cookie_data = getCookie("msg", request);
-  var content = ejs.render(index_page, {
-    title: "index",
-    content: msg,
-    data: data,
-    cookie_data: cookie_data,
-  });
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(content);
-  response.end();
-}
-
-function setCookie(key, value, response) {
-  var cookie = escape(value);
-  response.setHeader("Set-Cookie", [key + "=" + cookie]);
-}
-
-function getCookie(key, request) {
-  var cookie_data =
-    request.headers.cookie != undefined ? request.headers.cookie : "";
-  var data = cookie_data.split(";");
-  for (var i in data) {
-    if (data[i].trim().startsWith(key + "=")) {
-      var result = data[i].trim().substring(key.length + 1);
-      return unescape(result);
+"use strict";
+var http = require("http");
+var fs = require("fs");
+var ejs = require("ejs");
+var url = require("url");
+var qs = require("querystring");
+var index_page = fs.readFileSync("./index.ejs", "utf8");
+var login_page = fs.readFileSync("./login.ejs", "utf8");
+var max_num = 10; // 最大保管数
+var filename = "mydata.txt"; // データファイル名
+var message_data; // データ
+readFromFile(filename);
+var server = http.createServer(getFromClient);
+server.listen(3000);
+console.log("Server start!");
+// ここまでメインプログラム==========
+// createServerの処理
+function getFromClient(request, response) {
+    var url_parts = url.parse(request.url, true);
+    switch (url_parts.pathname) {
+        case "/": // トップページ（メッセージボード）
+            response_index(request, response);
+            break;
+        case "/login": // ログインページ
+            response_login(request, response);
+            break;
+        default:
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.end("no page...");
+            break;
     }
-  }
-  return ``;
 }
-
-function responseOther(request, response) {
-  var msg = "これがOtherページです";
-  if (request.method == "POST") {
-    var body = "";
-    request.on("data", (data) => {
-      body += data;
-    });
-    request.on("end", () => {
-      var post_data = qs.parse(body);
-      msg = `あなたは${post_data.msg}と書きました`;
-      var content = ejs.render(other_page, {
-        title: "other",
+// loginのアクセス処理
+function response_login(_, response) {
+    var content = ejs.render(login_page, {});
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(content);
+    response.end();
+}
+// indexのアクセス処理
+function response_index(request, response) {
+    // POSTアクセス時の処理
+    if (request.method == "POST") {
+        var body = "";
+        // データ受信のイベント処理
+        request.on("data", function (data) {
+            body += data;
+        });
+        // データ受信終了のイベント処理
+        request.on("end", function () {
+            var data;
+            data = qs.parse(body);
+            addToData(data.id, data.msg, filename, request);
+            write_index(request, response);
+        });
+    }
+    else {
+        write_index(request, response);
+    }
+}
+// indexのページ作成
+function write_index(request, response) {
+    var msg = "※何かメッセージを書いて下さい。";
+    var content = ejs.render(index_page, {
+        title: "Index",
         content: msg,
-      });
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(content);
-      response.end();
-    });
-  } else {
-    var msg = "ページはありません";
-    var content = ejs.render(other_page, {
-      title: "other",
-      content: msg,
+        data: message_data,
+        filename: "data_item"
     });
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write(content);
     response.end();
-  }
+}
+// テキストファイルをロード
+function readFromFile(fname) {
+    fs.readFile(fname, "utf8", function (err, data) {
+        message_data = data.split("\n");
+    });
+}
+// データを更新
+function addToData(id, msg, fname, request) {
+    var obj = { id: id, msg: msg };
+    var obj_str = JSON.stringify(obj);
+    console.log("add data: " + obj_str);
+    message_data.unshift(obj_str);
+    if (message_data.length > max_num) {
+        message_data.pop();
+    }
+    saveToFile(fname);
+}
+// データを保存
+function saveToFile(fname) {
+    var data_str = message_data.join("\n");
+    fs.writeFile(fname, data_str, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
 }
